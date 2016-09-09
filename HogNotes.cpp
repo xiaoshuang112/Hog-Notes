5035ca84ce7a92daaeed71f99c5a93834a291ffa
xiaoshuang112 <warriordeng@163.com>
2016-09-09 20:02:53 +0800

    Add some advsies

--- HogNotes.cpp
+++ HogNotes.cpp
@@ -1,4 +1,5 @@
-int VL_GetHOGFeature(int *pFeature, unsigned char* pSrc,int iW,int iH)
+int VL_GetHOGFeature(int *pFeature, unsigned char* pSrc,int iW,int iH) 
+//图片读取时直接读入灰图，万万不可在内存中进行转换 ，特别是在多次重复计算时 
 {
 
 	//cv::HOGDescriptor tHog;
@@ -8,9 +9,9 @@ int VL_GetHOGFeature(int *pFeature, unsigned char* pSrc,int iW,int iH)
 	//img->imageData = (char*)pSrc;
 	//vector<float> descriptors;
 	//tHog.compute(img,descriptors,cv::Size(1,1), cv::Size(0,0));
-	cv::HOGDescriptor *hog=new cv::HOGDescriptor(cvSize(width_image_BP,height_image_BP),cvSize(16,16),cvSize(8,8),cvSize(8,8),9);   //具体意思见参考文章1,2
-	vector<float>descriptors(3780);//结果数组
-	hog->compute(img, descriptors,cv::Size(1,1), cv::Size(0,0)); //调用计算函数开始计算
+	cv::HOGDescriptor *hog=new cv::HOGDescriptor(cvSize(width_image_BP,height_image_BP),cvSize(16,16),cvSize(8,8),cvSize(8,8),9);   //戮脽脤氓脪芒脣录录没虏脦驴录脦脛脮脗1,2
+	vector<float>descriptors(3780);//陆谩鹿没脢媒脳茅
+	hog->compute(img, descriptors,cv::Size(1,1), cv::Size(0,0)); //碌梅脫脙录脝脣茫潞炉脢媒驴陋脢录录脝脣茫
 
 	//cvReleaseImage(&img);
 	delete hog;
